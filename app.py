5ba669d9f59fc1d1adecd37632e7c613a4cdbeed
kumarhero6237-cyber <kumarhero6237@gmail.com>
2026-09-19 20:50:06 +0530

    Update JWT API base URL to secure HTTPS

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ OB_VERSION     = "OB54"
 LOGIN_URL      = "https://loginbp.ggblueshark.com"
 
 # ---------- JWT Token API ----------
-JWT_API_BASE = "http://38.29.171.32:2020/token"
+JWT_API_BASE = "https://jwtob55.vercel.app/token"
 
 # ---------- Workers / Burst ----------
 JWT_WORKERS   = 60
